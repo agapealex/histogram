ec92ec3c5b0b3f8749775eab85ef74cada188e18
agapealex <agapee.alex@yahoo.com>
2023-01-08 17:17:00 +0200

    add histogram

--- histogram-gfb/src/components/histogram/histogram.jsx
+++ histogram-gfb/src/components/histogram/histogram.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { useQuery} from '@apollo/client';
+import { scaleLinear, scaleBand } from '@visx/scale';
 import StyledHistogram from './histogram.style';
+import { Group } from '@visx/group';
+import { Grid } from '@visx/grid';
+import { Bar } from '@visx/shape';
 import { 
     GET_ALL_USERS,
-
+    convertPosts,
+    getPostsPerEachMonth,
+    getMaxMonth,
+    styles
 } from './utils';
 
 const Histogram = () => {
@@ -13,9 +20,73 @@ const Histogram = () => {
     if (loading) return 'Loading...';
     if (error) return `Error! ${error.message}`;
 
+    const allPosts = data.allPosts.map(convertPosts);
+    const postsPerEachMonth = getPostsPerEachMonth(allPosts);
+    const maxMonth = getMaxMonth(postsPerEachMonth, allPosts);
+    
+    const xMax = styles.width - styles.margin.left - styles.margin.right;
+    const yMax = styles.height - styles.margin.top - styles.margin.bottom;
+
+    const x = d => d.month;
+    const y = d => d.posts;
+    const c = (post, index) => index+1
+
+    const xScale = scaleBand({
+        range: [0, xMax],
+        round: true,
+        domain: postsPerEachMonth.map(x),
+        padding: 0.4,
+    });
+    
+    const dateScale = scaleBand({
+        domain: postsPerEachMonth.map(x),
+        padding: 0.2,
+    });
+
+    const postsScale = scaleLinear({
+        range: [yMax, 0],
+        domain: [0,maxMonth.length],
+        round: true, 
+    });
+
+    dateScale.rangeRound([0, xMax]);
+    postsScale.rangeRound([yMax, 0]);
+
+    const compose = (scale, accessor) => data => scale(accessor(data));
+    const xPoint = compose(xScale, x);
+    const yPoint = compose(postsScale, y);
+
     return (
         <StyledHistogram>
-            <p>txt</p>
+            <svg width={styles.width} height={styles.height} >
+                <rect x={0} y={0} width={styles.width} height={styles.height} fill={styles.color.background} rx={14} />
+                <Grid
+                    left={styles.margin.left}
+                    xScale={dateScale}
+                    yScale={postsScale}
+                    width={xMax}
+                    height={yMax}
+                    stroke="black"
+                    strokeOpacity={0.1}
+                />
+                {postsPerEachMonth.map((d, i) => {
+                        const barHeight = yMax - yPoint(d);
+
+                    return (
+                    <Group key={`bar-${i}`}  left={styles.margin.left}  >
+
+                        <Bar
+                            x={xPoint(d)}
+                            y={yPoint(d)}
+                            height={barHeight }
+                            width={xScale.bandwidth()}
+                            fill="blue"
+                        />
+                    </Group>
+                    );
+                })}
+
+            </svg>
         </StyledHistogram>
     )
 }
--- histogram-gfb/src/components/histogram/utils.js
+++ histogram-gfb/src/components/histogram/utils.js
@@ -1,4 +1,5 @@
 import { gql } from '@apollo/client';
+import moment from 'moment';
 
 export const GET_ALL_USERS = gql`
   query{
@@ -8,4 +9,50 @@ export const GET_ALL_USERS = gql`
     }
   }
 `;
+
+export const initializedPostsPerEachMonth = moment
+            .monthsShort()
+            .map( month => Object.freeze({
+                month,
+                posts: 0
+            }))
+
+export const convertPosts = (post) => {
+    const formatedDate = new Date(post.createdAt * 1);
+
+    return {
+        id: post.id,
+        monthCreated: initializedPostsPerEachMonth[formatedDate.getMonth()].month,
+        yearCreated: formatedDate.getFullYear()
+    }
+}
+
+const updatePostsPerEachMonth = (postsPerEachMonth, post) => {
+    post.yearCreated === 2019 &&
+    (postsPerEachMonth.find(p => p.month === post.monthCreated))
+    .posts++;
+    
+    return postsPerEachMonth;
+}
+
+export const getPostsPerEachMonth = (allPosts) =>{
+    return allPosts.reduce(updatePostsPerEachMonth, structuredClone(initializedPostsPerEachMonth));
+}
+
+export const getMaxMonth = (postsPerEachMonth, allPosts) => {
+    const maxMonth = postsPerEachMonth.reduce(function(prevPost, currentPost) {
+        return (prevPost.posts > currentPost.posts) ? prevPost : currentPost;
+    });
+
+    return  allPosts.filter( post => post.yearCreated === 2019 && post.monthCreated === maxMonth.month); 
+}
+export const styles ={
+    width: 500,
+    height: 500,
+    margin: { top: 20, bottom: 20, left: 20, right: 20 },
+    color:{
+        purple3: '#a44afe',
+        background : '#eaedff',
+    }
+}
 
